63c60bd3e036317907013b2ca5cc3567bf3e5b34
Naptoss <tomgabinio@gmail.com>
2023-09-30 18:21:44 -0300

    .

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
+const taskRoutes_1 = __importDefault(require("./routes/taskRoutes"));
+const app = (0, express_1.default)();
+const port = 3000;
+app.use(body_parser_1.default.json());
+app.use('/api/tasks', taskRoutes_1.default);
+app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+});
