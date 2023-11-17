2f8319edb134604f500d5bc3cfe60c45b3c70045
jnicklr <jnichollaslr@gmail.com>
2023-11-16 21:16:36 -0300

    Definindo os diretórios

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from  'express';
+import express from 'express';
 import bodyParser from 'body-parser';
 import taskRoutes from './routes/taskRoutes';
 import mongoose  from 'mongoose';
